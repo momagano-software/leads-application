f9ae43808f15a357f639f1a7aee4fd314ea726ae
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2022-02-27 23:01:32 +0200

    created: company registration component - wip

--- web/src/main/web/src/app/app.module.ts
+++ web/src/main/web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyRegistrationComponent } from './company-registration/company-registration.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyRegistrationComponent,
   ],
   imports: [
     BrowserModule,
