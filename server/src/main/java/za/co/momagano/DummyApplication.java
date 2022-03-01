203a1fd0d6437d514d9e48a5eb674672d1ae694d
Bakwanyana Thobela <bmthobela@gmail.com>
2022-03-01 22:30:43 +0200

    Enabled swagger

--- server/src/main/java/za/co/momagano/DummyApplication.java
+++ server/src/main/java/za/co/momagano/DummyApplication.java
@@ -1,8 +1,11 @@
 package za.co.momagano;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableSwagger2
 @SpringBootApplication
 public class DummyApplication {
     public static void main(String[] args) {
---
+++ server/src/main/java/za/co/momagano/configuration/SwaggerConfiguration.java
@@ -0,0 +1,29 @@
+package za.co.momagano.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("za.co.momagano"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addRedirectViewController("/", "/swagger-ui.html")
+//                .setContextRelative(true);
+//    }
+}
